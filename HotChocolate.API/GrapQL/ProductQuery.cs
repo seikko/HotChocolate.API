9d416930ce31791ee8e6a0feeedf0bbc723a0c2b
serkan akcay <Sakcay415@gmail.com>
2022-10-22 20:57:40 +0300

    Add project files.

---
+++ HotChocolate.API/GrapQL/ProductQuery.cs
@@ -0,0 +1,18 @@
+﻿using HotChocolate.API.Models;
+
+namespace HotChocolate.API.GrapQL
+{
+    public class ProductQuery
+    {
+        public Product GetProducts()
+        {
+            return new Product
+            {
+                ProductName = "Test Ürünü",
+                Description = "GrapQL Test ürünü",
+                Price = 10,
+                Stock = 10
+            };
+        }
+    }
+}
---
+++ HotChocolate.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace HotChocolate.API.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        
+    }
+}
